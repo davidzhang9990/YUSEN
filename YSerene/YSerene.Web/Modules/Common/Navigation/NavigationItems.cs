ff2897a27c75038a5362f13c03cc370af094e660
davidzhang706 <davidzhangtao@outlook.com>
2017-12-06 09:05:46 +0800

    导航菜单位置调整

--- YSerene/YSerene.Web/Modules/Common/Navigation/NavigationItems.cs
+++ YSerene/YSerene.Web/Modules/Common/Navigation/NavigationItems.cs
@@ -1,4 +1,15 @@
 ﻿using Serenity.Navigation;
 using Administration = YSerene.Administration.Pages;
+using MyPages = YSerene.Default.Pages;
 
-[assembly: NavigationLink(1000, "Dashboard", url: "~/", permission: "", icon: "fa-tachometer")]
+//[assembly: NavigationLink(1000, "Dashboard", url: "~/", permission: "", icon: "fa-tachometer")]
+
+[assembly: NavigationMenu(2000, "报名管理", icon: "fa-file-text-o")]
+[assembly: NavigationLink(2100, "报名管理/报名列表", typeof(MyPages.EnListController), icon: null)]
+
+//[assembly: NavigationMenu(9000, "Administration", icon: "fa-desktop")]
+//[assembly: NavigationLink(9000, "Administration/Exceptions Log", url: "~/errorlog.axd", permission: YSerene.Administration.PermissionKeys.Security, icon: "icon-ban", Target = "_blank")]
+//[assembly: NavigationLink(9000, "Administration/Languages", typeof(Administration.LanguageController), icon: "fa-comments")]
+//[assembly: NavigationLink(9000, "Administration/Translations", typeof(Administration.TranslationController), icon: "fa-comment-o")]
+//[assembly: NavigationLink(9000, "Administration/Roles", typeof(Administration.RoleController), icon: "fa-lock")]
+//[assembly: NavigationLink(9000, "Administration/User Management", typeof(Administration.UserController), icon: "fa-users")]
--- YSerene/YSerene.Web/Modules/Default/DefaultNavigation.cs
+++
@@ -1,4 +0,0 @@
-﻿using Serenity.Navigation;
-using MyPages = YSerene.Default.Pages;
-
-[assembly: NavigationLink(int.MaxValue, "Default/En List", typeof(MyPages.EnListController), icon: null)]
